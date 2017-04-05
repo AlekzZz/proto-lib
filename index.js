0e30c74b2e7e84eda2030bcf08a482baad8534b0
Aleksandr Strutynskyy <alekzzz@gmail.com>
2017-04-04 22:14:50 -0700

    Port node version lib

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const Controller = require('./lib/controller');
 const Model = require('./lib/model');
+const View = require('./lib/view');
+const Controller = require('./lib/controller');
 const Event = require('./lib/event');
 const Helpers = require('./lib/helpers');
 
 module.exports = {
-  Controller,
   Model,
+  View,
+  Controller,
   Event,
   Helpers
 };
